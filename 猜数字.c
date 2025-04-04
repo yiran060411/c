ea521de88c9a13cfb7edc12920f3b227bb121f50
yiran <nyr060411@qq.com>
2025-04-04 15:15:13 +0800

    6

---
+++ 猜数字.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main(void){
+    printf("%d\n",RAND_MAX);
+    srand((int)time(NULL));
+    int magicnum = rand()*1.0/RAND_MAX*100;// 生成随机数
+    int num;
+    printf("input your number:");
+    scanf("%d",&num);
+    while(magicnum != num){
+        if (magicnum < num)
+            printf("your number is too big\n");
+        else
+            printf("your number is too small\n");
+        printf("input your number:");
+        scanf("%d",&num);
+    }
+    printf("you are right\n");
+    return 0;
+}
+   
+
+    
